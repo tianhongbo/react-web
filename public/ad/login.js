e43e5954377ffa83d107a8a08fd62f2f8e71cf2f
Lv <andy.lv@intel.com>
2015-12-28 17:06:52 +0800

    change to es6

--- public/ad/login.js
+++ public/ad/login.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import React from 'React';
+
 var LoginSelector = React.createClass({
   signIn: function () {
     var email = this.refs.email.getDOMNode().value.trim();
@@ -16,33 +18,35 @@ var LoginSelector = React.createClass({
   },
   render: function () {
     return (
-      <article className="hold-transition login-page loginSelector">
-        <div className="login-box">
-          <div className="login-logo">
-            React - Web
-          </div>
-          <div className="login-box-body">
-            <p className="login-box-msg">Sign in to start your session</p>
+        
+        
+    //   <article className="hold-transition login-page loginSelector">
+    //     <div className="login-box">
+    //       <div className="login-logo">
+    //         React - Web
+    //       </div>
+    //       <div className="login-box-body">
+    //         <p className="login-box-msg">Sign in to start your session</p>
 
-            <div className="form-group has-feedback">
-              <input type="email" className="form-control" placeholder="Email" ref="email"/>
-              <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
-            </div>
+    //         <div className="form-group has-feedback">
+    //           <input type="email" className="form-control" placeholder="Email" ref="email"/>
+    //           <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
+    //         </div>
 
-            <div className="form-group has-feedback">
-              <input type="password" className="form-control" placeholder="Password" ref="password"/>
-              <span className="glyphicon glyphicon-lock form-control-feedback"></span>
-            </div>
+    //         <div className="form-group has-feedback">
+    //           <input type="password" className="form-control" placeholder="Password" ref="password"/>
+    //           <span className="glyphicon glyphicon-lock form-control-feedback"></span>
+    //         </div>
 
-            <div className="row">
-              <div className="col-xs-12">
-                <button onClick={this.signIn} type="submit" className=" btn btn-primary btn-block btn-flat">Sign In
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </article>
+    //         <div className="row">
+    //           <div className="col-xs-12">
+    //             <button onClick={this.signIn} type="submit" className=" btn btn-primary btn-block btn-flat">Sign In
+    //             </button>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </article>
     );
   }
 });
